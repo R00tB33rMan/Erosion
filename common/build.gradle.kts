7876237174bdc9a937ab4a1bd926da4e695afc85
R00tB33rMan <36140389+R00tB33rMan@users.noreply.github.com>
2023-11-10 10:00:46 -0500

    Upgrade Various Dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,19 +3,19 @@ plugins {
 }
 
 dependencies {
-    api("io.netty", "netty-transport", "4.1.43.Final")
-    api("io.netty", "netty-handler", "4.1.43.Final")
-    api("io.netty", "netty-transport-native-epoll", "4.1.43.Final")
+    api("io.netty", "netty-transport", "4.1.101.Final")
+    api("io.netty", "netty-handler", "4.1.101.Final")
+    api("io.netty", "netty-transport-native-epoll", "4.1.101.Final")
     // TODO just needed for VarInt encoding. Might be wholly unnecessary.
     api("org.cloudburstmc.protocol", "common", "3.0.0.Beta1-SNAPSHOT")
     // TODO use release.
-    api("com.github.steveice10", "opennbt", "1.5-SNAPSHOT")
+    api("com.github.steveice10", "opennbt", "1.6-SNAPSHOT")
     api("org.cloudburstmc.math", "immutable", "2.0")
-    api("org.cloudburstmc", "nbt", "3.0.0.Final")
+    api("org.cloudburstmc", "nbt", "3.0.2.Final")
 
-    api("com.nukkitx.fastutil", "fastutil-int-object-maps", "8.5.3")
-    api("com.nukkitx.fastutil", "fastutil-object-int-maps", "8.5.3")
-    implementation("org.jetbrains:annotations:20.1.0")
+    api("com.nukkitx.fastutil", "fastutil-int-object-maps", "8.5.13")
+    api("com.nukkitx.fastutil", "fastutil-object-int-maps", "8.5.13")
+    implementation("org.jetbrains:annotations:24.0.1")
 }
 
 java {
